feb9a5aae2ecc7c858898966fc40bcaa1afddcad
Shivan Kathir <shivan.kathir@gmail.com>
2024-12-29 16:58:20 -0500

    installed react router and intialized different pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Register from "./pages/Register"
+// import Login from "./pages/Login"
+// import Write from "./pages/Write"
+// import Home from "./pages/Home"
+// import Single from "./pages/Single"
+
+const router = <BrowserRouter>
+<Routes>
+  <Route path="/" element={<div>This is Home </div>} />
+</Routes>
+<Routes>
+  <Route path="/register" element={<Register/>} />
+</Routes>
+</BrowserRouter>
+
 function App() {
-  return <div>Hello World!</div>;
+  return <div>{router }</div>;
 }
 
 export default App;
