1b15c22c81d8aa808e7c08a84cc1c36447d21675
Shivan Kathir <shivan.kathir@gmail.com>
2024-12-29 17:16:30 -0500

    create home page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,45 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router";
 import Register from "./pages/Register"
-// import Login from "./pages/Login"
-// import Write from "./pages/Write"
-// import Home from "./pages/Home"
-// import Single from "./pages/Single"
+import Login from "./pages/Login"
+import Write from "./pages/Write"
+import Home from "./pages/Home"
+import Single from "./pages/Single"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
-const router = <BrowserRouter>
-<Routes>
-  <Route path="/" element={<div>This is Home </div>} />
-</Routes>
-<Routes>
-  <Route path="/register" element={<Register/>} />
-</Routes>
-</BrowserRouter>
+const Layout = ()=>{
+  return(
+    <div>
+      <Navbar/>
+      <Outlet/>
+      <Footer/> 
+    </div>
+  )
+}
+
+const router = (
+  <BrowserRouter>
+    <Routes>
+      {/* Layout Route with Nested Routes */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} /> {/* This is equivalent to path="/" */}
+        <Route path="/post/:id" element={<Single />} />
+        <Route path="write" element={<Write />} />
+      </Route>
+      {/* Register Route */}
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 function App() {
-  return <div>{router }</div>;
+  return <div className="app">
+    <div className="container">
+      {router }
+    </div>
+    </div>;
 }
+
+
 
 export default App;
