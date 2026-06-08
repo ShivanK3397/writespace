a0850123937f8f5c2ada6b63572a3541825cbf0f
Shivan Kathir <shivan.kathir@gmail.com>
2026-06-08 15:42:59 -0400

    routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,7 @@ import { register,login,logout } from "../controllers/auth.js"
 const router = express.Router()
 
 router.post("/register",register)
-router.post("/login",login)
+router.post("/login",login) 
 router.post("/logout",logout)
 
 export default router
